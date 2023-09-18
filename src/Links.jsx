fa961007bd228e88cfac94e6ce48797f61dfae8e
Jeyadev-Muthukumarasamy <jeydev007@gmail.com>
2023-09-18 21:48:06 +0530

    first

--- src/Links.jsx
+++ src/Links.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Links = () => {
   return (
     <div>
-      <div id="appcontainer">
+      <div id="appcontaines">
         
       </div>
     </div>
